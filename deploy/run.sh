d12e13cf6dd804bac027d61f12e403127cbea82f
gzwyyz01 <443949916@qq.com>
2026-09-23 02:08:39 +0800

    Update run.sh

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+
 # 抖音云托管要求：项目必须包含 run.sh，且打包到 /opt/application/ 目录
-# 作用：以前台方式启动 Nginx（云托管靠该进程保活）
+# 作用：以前台方式启动 Node 静态服务器（云托管靠该进程保活）
 set -e
-exec nginx -g 'daemon off;'
+exec node /opt/application/server.js
